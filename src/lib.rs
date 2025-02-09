b259d30b25b77f7c2c5f39b6b0949e4efee34d70
khai96_ <hvksmr1996@gmail.com>
2025-02-09 18:09:17 +0700

    chore: switch to `tar` as we must allocate anyway

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
-#[cfg(feature = "std")]
 pub use arch_pkg_text::indexmap;
 #[cfg(feature = "parking_lot")]
 pub use arch_pkg_text::parking_lot;
 pub use arch_pkg_text::{desc, parse, value};
 pub use libflate;
-pub use tar_no_std;
