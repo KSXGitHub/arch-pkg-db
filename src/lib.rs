78c7bae8369f83922a380bf9ae5db1dc107bdc24
khai96_ <hvksmr1996@gmail.com>
2025-02-09 15:59:49 +0700

    style: remove some empty lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use arch_pkg_text::{desc, parse, value};
-
 #[cfg(feature = "std")]
 pub use arch_pkg_text::indexmap;
-
 #[cfg(feature = "parking_lot")]
 pub use arch_pkg_text::parking_lot;
+pub use arch_pkg_text::{desc, parse, value};
