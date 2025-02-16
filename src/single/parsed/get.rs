e3e1cd7816318459a3185dee14d5a2eae9f827cc
khai96_ <hvksmr1996@gmail.com>
2025-02-16 14:41:48 +0700

    docs: fix wordings

--- src/single/parsed/get.rs
+++ src/single/parsed/get.rs
@@ -2,12 +2,12 @@ use super::SingleParsedDatabase;
 use arch_pkg_text::value::Name;
 
 impl<'a, Querier> SingleParsedDatabase<'a, Querier> {
-    /// Get an immutable reference to a `desc` file by package name.
+    /// Get an immutable reference to a querier by package name.
     pub fn get(&self, name: Name<'a>) -> Option<&'_ Querier> {
         self.internal.get(&name)
     }
 
-    /// Get a `desc` file with mutable querier by package name.
+    /// Get a mutable reference to a querier by package name.
     pub fn get_mut(&mut self, name: Name<'a>) -> Option<&mut Querier> {
         self.internal.get_mut(&name)
     }
