2aa230fd52e638a78eb5843177ae71ce3191cb9e
khai96_ <hvksmr1996@gmail.com>
2025-02-23 00:34:25 +0700

    feat: `extend_from_tar`

--- src/single/archive.rs
+++ src/single/archive.rs
@@ -1,7 +1,10 @@
 mod convert;
 mod insert;
+mod load;
 mod misc;
 mod new;
+
+pub use load::LoadTarError;
 
 /// Collections of all `desc` texts from which queriers may access data.
 #[derive(Debug, Default, Clone)]
---
+++ src/single/archive/load.rs
@@ -0,0 +1,40 @@
+use super::Archive;
+use derive_more::{Display, Error};
+use pipe_trait::Pipe;
+use std::{
+    ffi::OsStr,
+    io::{self, Read},
+};
+
+/// Error when trying to load data from a tar archive.
+#[derive(Debug, Display, Error)]
+#[display("Failed to read the tar archive: {_0}")]
+pub struct LoadTarError(io::Error);
+
+impl Archive {
+    /// Extract a tar archive and add contents from `desc` files to the archive.
+    pub fn extend_from_tar(&mut self, raw_tar_bytes: &[u8]) -> Result<(), LoadTarError> {
+        let mut tar = tar::Archive::new(raw_tar_bytes);
+        let entries = tar.entries().map_err(LoadTarError)?;
+
+        for entry in entries {
+            let mut entry = entry.map_err(LoadTarError)?;
+            let path = entry.path().map_err(LoadTarError)?;
+            let file_name = path.file_name().and_then(OsStr::to_str);
+            if file_name != Some("desc") {
+                continue;
+            }
+            let mut text = entry
+                .header()
+                .size()
+                .unwrap_or(0)
+                .pipe(usize::try_from)
+                .unwrap_or(0)
+                .pipe(String::with_capacity);
+            entry.read_to_string(&mut text).map_err(LoadTarError)?;
+            self.insert(text);
+        }
+
+        Ok(())
+    }
+}
