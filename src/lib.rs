d243c6cc61229a889a9a5978887f2f0ad1c6caa9
khai96_ <hvksmr1996@gmail.com>
2025-02-22 15:30:05 +0700

    chore(deps): update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod db;
 pub use db::{Insert, Lookup, LookupMut, PackageDatabase};
 
+pub mod misc;
 pub mod single;
 
 pub use single::SingleParsedDatabase;
 
-pub use arch_pkg_text::indexmap;
-#[cfg(feature = "parking_lot")]
-pub use arch_pkg_text::parking_lot;
 pub use arch_pkg_text::{desc, parse, value};
---
+++ src/misc.rs
@@ -0,0 +1,5 @@
+//! Miscellaneous items.
+
+#[cfg(feature = "parking_lot")]
+pub use arch_pkg_text::misc::parking_lot;
+pub use arch_pkg_text::misc::{False, StaticBool, True, desc, indexmap, typebool};
