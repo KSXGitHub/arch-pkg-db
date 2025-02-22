284af39e05a3fd84dff3b6e2b9428fffe73744dc
khai96_ <hvksmr1996@gmail.com>
2025-02-23 00:39:12 +0700

    refactor: rename `load` to `tar`

--- src/single/archive.rs
+++ src/single/archive.rs
@@ -1,10 +1,10 @@
 mod convert;
 mod insert;
-mod load;
 mod misc;
 mod new;
+mod tar;
 
-pub use load::LoadTarError;
+pub use tar::LoadTarError;
 
 /// Collections of all `desc` texts from which queriers may access data.
 #[derive(Debug, Default, Clone)]
