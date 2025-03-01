e3563123f397895c94363acfbbc65f5c8a8932ef
khai96_ <hvksmr1996@gmail.com>
2025-03-02 04:45:33 +0700

    feat: `add_item`

--- src/single/text/insert.rs
+++ src/single/text/insert.rs
@@ -5,4 +5,10 @@ impl TextCollection {
     pub fn insert(&mut self, text: Text) {
         self.internal.push(text);
     }
+
+    /// Add data into the text collection.
+    pub fn add_item(mut self, text: Text) -> Self {
+        self.insert(text);
+        self
+    }
 }
