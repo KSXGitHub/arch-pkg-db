d2fd6d3b84bd382560c8187aa892e295b3f3c345
khai96_ <hvksmr1996@gmail.com>
2025-03-01 16:06:06 +0700

    feat!: generic parse

--- src/single/text/parse.rs
+++ src/single/text/parse.rs
@@ -1,6 +1,9 @@
 use super::TextCollection;
-use crate::{EagerQueryDatabase, single::query::AddError};
-use arch_pkg_text::{ParsedDesc, parse::DescParseError};
+use crate::{QueryDatabase, single::query::AddError};
+use arch_pkg_text::desc::{
+    QueryMut,
+    misc::{ReuseAdvice, True},
+};
 use derive_more::{Display, Error};
 
 /// Error type when trying to create a [`QueryDatabase`] from a [`TextCollection`].
@@ -13,18 +16,26 @@ pub enum TextCollectionParseError<Querier, ParseError> {
     Add(AddError<Querier>),
 }
 
-/// Return type of [`TextCollection::parse_eager`].
-type ParseEagerResult<'a> =
-    Result<EagerQueryDatabase<'a>, TextCollectionParseError<ParsedDesc<'a>, DescParseError<'a>>>;
+/// Return type of [`TextCollection::parse`].
+type ParseResult<'a, Querier> = Result<
+    QueryDatabase<'a, Querier>,
+    TextCollectionParseError<Querier, <&'a str as TryInto<Querier>>::Error>,
+>;
 
 impl TextCollection {
-    /// Parse a database of eager queriers.
-    #[expect(clippy::result_large_err)] // until `parse_*_with_issue`.
-    pub fn parse_eager(&self) -> ParseEagerResult<'_> {
-        let mut db = EagerQueryDatabase::new();
+    /// Parse a database of queriers.
+    pub fn parse<'a, Querier>(&'a self) -> ParseResult<'a, Querier>
+    where
+        &'a str: TryInto<Querier>,
+        Querier: QueryMut<'a> + ReuseAdvice<ShouldReuse = True>,
+    {
+        let mut db = QueryDatabase::new();
 
         for text in &self.internal {
-            let querier = ParsedDesc::parse(text).map_err(TextCollectionParseError::Parse)?;
+            let querier = text
+                .as_str()
+                .try_into()
+                .map_err(TextCollectionParseError::Parse)?;
             db.add(querier).map_err(TextCollectionParseError::Add)?;
         }
 
