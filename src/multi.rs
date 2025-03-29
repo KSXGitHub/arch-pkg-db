1be4a2b5d95a10ffa8c9fecb5ac7fd059337a7f9
khai96_ <hvksmr1996@gmail.com>
2025-03-29 19:48:59 +0700

    feat: re-export more

--- src/multi.rs
+++ src/multi.rs
@@ -4,5 +4,8 @@ mod repository;
 
 pub mod query;
 
-pub use query::{EagerMultiQueryDatabase, MemoMultiQueryDatabase, MultiQueryDatabase};
+pub use query::{
+    EagerMultiQueryDatabase, EagerMultiQueryDatabaseLatest, MemoMultiQueryDatabase,
+    MemoMultiQueryDatabaseLatest, MultiQuerier, MultiQueryDatabase, MultiQueryDatabaseLatest,
+};
 pub use repository::RepositoryName;
