71e8788b2892deafd23c0a9f436df936dc1c3764
khai96_ <hvksmr1996@gmail.com>
2025-05-18 07:39:56 +0700

    docs: fix wording

--- src/single/query/iter/from.rs
+++ src/single/query/iter/from.rs
@@ -5,7 +5,7 @@ use arch_pkg_text::{
 };
 
 impl<'a, Querier: ShouldReuse> QueryDatabase<'a, Querier> {
-    /// Construct the database with an iterator of queriers of `desc` files.
+    /// Construct a database with an iterator of queriers of `desc` files.
     fn from_queriers_with<QuerierIter, Insert, InsertSuccess, InsertError>(
         queriers: QuerierIter,
         mut insert: Insert,
@@ -25,7 +25,7 @@ impl<'a, Querier: ShouldReuse> QueryDatabase<'a, Querier> {
         Ok(db)
     }
 
-    /// Construct the database with an iterator of [immutable queriers](Query) of `desc` files.
+    /// Construct a database with an iterator of [immutable queriers](Query) of `desc` files.
     ///
     /// If there are collision between queriers in [name](arch_pkg_text::value::Name), the later querier would override the earlier.
     pub fn from_queriers<QuerierIter>(queriers: QuerierIter) -> Result<Self, InsertError>
@@ -36,7 +36,7 @@ impl<'a, Querier: ShouldReuse> QueryDatabase<'a, Querier> {
         QueryDatabase::from_queriers_with(queriers, QueryDatabase::insert)
     }
 
-    /// Construct the database with an iterator of [mutable queriers](QueryMut) of `desc` files.
+    /// Construct a database with an iterator of [mutable queriers](QueryMut) of `desc` files.
     ///
     /// If there are collision between queriers in [name](arch_pkg_text::value::Name), the later querier would override the earlier.
     pub fn from_queriers_mut<QuerierIter>(queriers: QuerierIter) -> Result<Self, InsertError>
@@ -47,7 +47,7 @@ impl<'a, Querier: ShouldReuse> QueryDatabase<'a, Querier> {
         QueryDatabase::from_queriers_with(queriers, QueryDatabase::insert_mut)
     }
 
-    /// Construct the database from an iterator of [immutable queriers](Query) of `desc` files.
+    /// Construct a database from an iterator of [immutable queriers](Query) of `desc` files.
     ///
     /// If there are collision between queriers in [name](arch_pkg_text::value::Name), the one with newer
     /// [package version](arch_pkg_text::value::Version) would override the older.
@@ -61,7 +61,7 @@ impl<'a, Querier: ShouldReuse> QueryDatabase<'a, Querier> {
         QueryDatabase::from_queriers_with(queriers, QueryDatabase::insert_newer)
     }
 
-    /// Construct the database from an iterator of [mutable queriers](QueryMut) of `desc` files.
+    /// Construct a database from an iterator of [mutable queriers](QueryMut) of `desc` files.
     ///
     /// If there are collision between queriers in [name](arch_pkg_text::value::Name), the one with newer
     /// [package version](arch_pkg_text::value::Version) would override the older.
