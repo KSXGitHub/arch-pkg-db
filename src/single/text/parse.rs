b723104e48e092a6fe5bc8708456ef7fcdc55db7
khai96_ <hvksmr1996@gmail.com>
2025-05-24 19:27:46 +0700

    feat: TextCollection::par_parse[_mut]

--- src/single/text/parse.rs
+++ src/single/text/parse.rs
@@ -1,7 +1,9 @@
-use super::TextCollection;
+use super::{Text, TextCollection};
 use crate::{QueryDatabase, single::query::InsertError};
 use arch_pkg_text::desc::{Query, QueryMut, misc::ShouldReuse};
 use derive_more::{Display, Error};
+use pipe_trait::Pipe;
+use rayon::prelude::*;
 
 /// Error type when trying to create a [`QueryDatabase`] from a [`TextCollection`].
 ///
@@ -55,5 +57,43 @@ impl TextCollection {
         Querier: QueryMut<'a> + ShouldReuse,
     {
         self.parse_with(|db, querier| db.insert_mut(querier).map(drop))
+    }
+
+    /// Parse a database of queriers in parallel.
+    fn par_parse_with<'a, Querier, QueriersIntoDb>(
+        &'a self,
+        queriers_into_db: QueriersIntoDb,
+    ) -> ParseResult<'a, Querier>
+    where
+        &'a str: TryInto<Querier, Error: Send>,
+        Querier: Send,
+        QueriersIntoDb: FnOnce(Vec<Querier>) -> Result<QueryDatabase<'a, Querier>, InsertError>,
+    {
+        self.internal
+            .par_iter()
+            .map(Text::as_str)
+            .map(TryInto::<Querier>::try_into)
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(TextCollectionParseError::Parse)?
+            .pipe(queriers_into_db)
+            .map_err(TextCollectionParseError::Insert)
+    }
+
+    /// Parse a database of [immutable queriers](Query) in parallel.
+    pub fn par_parse<'a, Querier>(&'a self) -> ParseResult<'a, Querier>
+    where
+        &'a str: TryInto<Querier, Error: Send>,
+        Querier: Query<'a> + ShouldReuse + Send,
+    {
+        self.par_parse_with(QueryDatabase::from_queriers)
+    }
+
+    /// Parse a database of [mutable queriers](QueryMut) in parallel.
+    pub fn par_parse_mut<'a, Querier>(&'a self) -> ParseResult<'a, Querier>
+    where
+        &'a str: TryInto<Querier, Error: Send>,
+        Querier: QueryMut<'a> + ShouldReuse + Send,
+    {
+        self.par_parse_with(QueryDatabase::from_queriers_mut)
     }
 }
