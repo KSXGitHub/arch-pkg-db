d7399454abe988cdb03faa821cea9f714ff6509b
khai96_ <hvksmr1996@gmail.com>
2025-10-19 02:16:54 +0700

    docs(example): `parse_eager_query_db`

---
+++ examples/parse_eager_query_db.rs
@@ -0,0 +1,10 @@
+use arch_pkg_db::{TextCollection, desc::EagerQuerier};
+use pipe_trait::Pipe;
+
+fn main() {
+    let texts = "/var/lib/pacman/local/"
+        .pipe_as_ref(TextCollection::from_local_db)
+        .expect("load text collection");
+    let db = texts.par_parse::<EagerQuerier>().expect("parse queriers");
+    dbg!(&db);
+}
