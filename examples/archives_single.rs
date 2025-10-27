c8f8833dbbc61adf0fc974d6ccfaaf178ceb9d03
khai96_ <hvksmr1996@gmail.com>
2025-10-27 12:14:32 +0700

    docs(example): change wording

--- examples/archives_single.rs
+++ examples/archives_single.rs
@@ -2,7 +2,7 @@
 //!
 //! **What it does:**
 //! * Eagerly loading desc files from repository databases (`core.db`, `extra.db`, `multilib.db`) in `/var/lib/pacman/sync/` using
-//!   [`arch_pkg_db::TextCollection::extend_from_archive`] to extract archives (with automatic MIME type detection) into a single merged
+//!   [`arch_pkg_db::TextCollection::extend_from_archive`] to extract archives (with automatic MIME type detection) into a repository-unaware
 //!   database object of type [`arch_pkg_db::QueryDatabase`].
 //! * Eagerly parsing all loaded desc texts with [`arch_pkg_db::TextCollection::par_parse`] into [`arch_pkg_db::desc::EagerQuerier`] objects.
 //! * Asking for a package by name, looking it up using [`arch_pkg_db::QueryDatabase::get`].
