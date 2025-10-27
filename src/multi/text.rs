953fc853c99583bf27049d2d3df7532cb2196dde
khai96_ <hvksmr1996@gmail.com>
2025-10-27 19:36:16 +0700

    feat: re-export iterator types

--- src/multi/text.rs
+++ src/multi/text.rs
@@ -7,6 +7,8 @@ mod new;
 use super::RepositoryName;
 use crate::misc::Text;
 
+pub use iter::{MultiTextIntoIter, MultiTextIter, MultiTextIterMut};
+
 /// Collection of all `desc` texts and their corresponding [repository names](RepositoryName)
 /// from which queriers may access data.
 #[derive(Debug, Default, Clone)]
