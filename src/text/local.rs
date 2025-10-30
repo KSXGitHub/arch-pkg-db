9897a1a3f5309dfe7fb694221d8eeaf13c1794e5
khai96_ <hvksmr1996@gmail.com>
2025-10-30 23:32:19 +0700

    refactor!: opaque error types

--- src/text/local.rs
+++ src/text/local.rs
@@ -1,4 +1,5 @@
 use super::{Text, TextCollection};
+use core::error::Error;
 use derive_more::{Display, Error};
 use pipe_trait::Pipe;
 use rayon::prelude::*;
@@ -8,21 +9,74 @@ use std::{
     path::{Path, PathBuf},
 };
 
-/// Error when trying to load data from a local pacman database.
+/// Error when trying to read the directory of the local pacman database.
 #[derive(Debug, Display, Error)]
+#[display("Failed to read {path:?} as a directory: {error}")]
+pub struct ReadLocalDbDirError<'a> {
+    #[error(source)]
+    error: io::Error,
+    path: &'a Path,
+}
+
+impl<'a> ReadLocalDbDirError<'a> {
+    /// Create an error.
+    fn new(error: io::Error, path: &'a Path) -> Self {
+        Self { error, path }
+    }
+
+    /// The source of this error.
+    pub fn source(&self) -> &io::Error {
+        &self.error
+    }
+
+    /// Path to the directory of the local pacman database.
+    pub fn path(&self) -> &Path {
+        self.path
+    }
+}
+
+/// Error when trying to read a file in the local pacman database.
+#[derive(Debug, Display, Error)]
+#[display("Failed to read {path:?} as a text file: {error}")]
+pub struct ReadLocalDbFileError {
+    #[error(source)]
+    error: io::Error,
+    path: PathBuf,
+}
+
+impl ReadLocalDbFileError {
+    /// Create an error.
+    fn new(error: io::Error, path: PathBuf) -> Self {
+        Self { error, path }
+    }
+
+    /// The source of this error.
+    pub fn source(&self) -> &io::Error {
+        &self.error
+    }
+
+    /// Path to the directory of the local pacman database.
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+}
+
+/// Error when trying to load data from a local pacman database.
+#[derive(Debug, Display)]
 pub enum LoadLocalDbError<'a> {
-    #[display("Failed to read {path:?} as a directory: {error}")]
-    ReadDir {
-        #[error(source)]
-        error: io::Error,
-        path: &'a Path,
-    },
-    #[display("Failed to read {path:?} as a text file: {error}")]
-    ReadFile {
-        #[error(source)]
-        error: io::Error,
-        path: PathBuf,
-    },
+    ReadDir(ReadLocalDbDirError<'a>),
+    ReadFile(ReadLocalDbFileError),
+}
+
+// We implement Error manually because derive_more::Error was unable to handle it.
+// Issue: <https://github.com/JelteF/derive_more/issues/511>
+impl<'a> Error for LoadLocalDbError<'a> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            LoadLocalDbError::ReadDir(error) => Error::source(error),
+            LoadLocalDbError::ReadFile(error) => Error::source(error),
+        }
+    }
 }
 
 impl TextCollection {
@@ -33,10 +87,10 @@ impl TextCollection {
         &mut self,
         local_db_path: &'path Path,
     ) -> Result<(), LoadLocalDbError<'path>> {
-        let entries = read_dir(local_db_path).map_err(|error| LoadLocalDbError::ReadDir {
-            error,
-            path: local_db_path,
-        })?;
+        let entries = local_db_path
+            .pipe(read_dir)
+            .map_err(|error| ReadLocalDbDirError::new(error, local_db_path))
+            .map_err(LoadLocalDbError::ReadDir)?;
 
         for entry in entries {
             let Ok(entry) = entry else { continue };
@@ -51,10 +105,9 @@ impl TextCollection {
                 Ok(text) => self.insert(text.into()),
                 Err(error) if error.kind() == ErrorKind::NotFound => continue,
                 Err(error) => {
-                    return Err(LoadLocalDbError::ReadFile {
-                        error,
-                        path: file_path,
-                    });
+                    return ReadLocalDbFileError::new(error, file_path)
+                        .pipe(LoadLocalDbError::ReadFile)
+                        .pipe(Err);
                 }
             };
         }
@@ -86,10 +139,8 @@ impl TextCollection {
     ) -> Result<(), LoadLocalDbError<'path>> {
         let texts = local_db_path
             .pipe(read_dir)
-            .map_err(|error| LoadLocalDbError::ReadDir {
-                error,
-                path: local_db_path,
-            })?
+            .map_err(|error| ReadLocalDbDirError::new(error, local_db_path))
+            .map_err(LoadLocalDbError::ReadDir)?
             .par_bridge()
             .flatten()
             .filter(|entry| {
@@ -104,10 +155,9 @@ impl TextCollection {
                 match read_to_string(&file_path) {
                     Ok(text) => Ok(Some(text)),
                     Err(error) if error.kind() == ErrorKind::NotFound => Ok(None),
-                    Err(error) => Err(LoadLocalDbError::ReadFile {
-                        error,
-                        path: file_path,
-                    }),
+                    Err(error) => ReadLocalDbFileError::new(error, file_path)
+                        .pipe(LoadLocalDbError::ReadFile)
+                        .pipe(Err),
                 }
             })
             .collect::<Result<Vec<Option<String>>, LoadLocalDbError>>()?
