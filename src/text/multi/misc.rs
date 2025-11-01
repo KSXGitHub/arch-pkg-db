eb2772c54086db7bfd975bd2b6304f836ad2b49c
khai96_ <hvksmr1996@gmail.com>
2025-11-02 03:03:49 +0700

    docs: fix

--- src/text/multi/misc.rs
+++ src/text/multi/misc.rs
@@ -6,12 +6,12 @@ impl MultiTextCollection<'_> {
         self.internal.shrink_to_fit()
     }
 
-    /// The number of [`Text`](crate::Text)s within the multi-collection.
+    /// The number of [`TextCollection`](crate::TextCollection)s within the multi-collection.
     pub fn len(&self) -> usize {
         self.internal.len()
     }
 
-    /// Whether the multi-collection is empty.
+    /// Whether the multi-collection has any [`TextCollection`](crate::TextCollection).
     pub fn is_empty(&self) -> bool {
         self.internal.is_empty()
     }
